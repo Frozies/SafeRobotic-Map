708e1e52109e2799e43fd514a6ecdc4a638c6920
Frozies <52686922+Frozies@users.noreply.github.com>
2021-06-09 21:58:43 -0400

    Reupdated to latest mapbox versions. implemented map & basic drawing features

---
+++ components/Map/index.js
@@ -0,0 +1,60 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+const Map = () => {
+    const mapContainerRef = useRef(null);
+
+    const [lng, setLng] = useState(5); /*TODO: Set to current users location*/
+    const [lat, setLat] = useState(34);/*TODO: Set to current users location*/
+    const [zoom, setZoom] = useState(1.5);/*TODO: Set to current users location*/
+
+    const Draw = new MapboxDraw();
+
+    // Initialize map when component mounts
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        // Add navigation control (the +/- zoom buttons)
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+        map.addControl(Draw, 'top-left');
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+
+            {/*TODO: This is bad. move both of these imports somewhere else.*/}
+            <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.0/mapbox-gl.js'></script>
+            <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.0/mapbox-gl.css' rel='stylesheet' />
+
+            <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.css' type='text/css' />
+            <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.js'></script>
+
+            <div className='sidebarStyle'>
+                <div>
+                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                </div>
+            </div>
+            <div className='map-container' ref={mapContainerRef} />
+        </div>
+    );
+};
+
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-import ReactMapboxGl from "react-mapbox-gl";
-import DrawControl from "react-mapbox-gl-draw";
-import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-
-const Map = ReactMapboxGl({
-    accessToken:
-        "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g"
-});
-
+import Map from '../components/Map/index'
 
 export default function Home() {
-
-    const onDrawCreate = ({ features }) => {
-        console.log(features);
-    };
-
-    const onDrawUpdate = ({ features }) => {
-        console.log(features);
-    };
-
   return (
     <div className={styles.container}>
       <Head>
@@ -35,15 +17,8 @@ export default function Home() {
         </h1>
           <h2> Map Demo!</h2>
 
-          <Map
-              style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
-              containerStyle={{
-                  height: "600px",
-                  width: "100vw"
-              }}
-          >
-              <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate}  />
-          </Map>
+          {/*Draw Map here*/}
+          <Map/>
 
       </main>
     </div>
