d7ee1a5a3be3d0eb1f09159eb374d8be07b99820
Frozies <52686922+Frozies@users.noreply.github.com>
2021-07-05 13:06:30 -0400

    Refactored the map. Included search bar.

--- components/map/index.js
+++ components/map/index.js
@@ -1,33 +1,45 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import turf from "turf";
-import useCurrentLocation from "../hooks/useCurrentLocation";
-import useWatchLocation from "../hooks/useWatchLocation";
-import Location from "./Location";
-import {geolocationOptions} from "../constants/geolocationOptions";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, {useState, useRef, useCallback, useEffect} from "react";
+import MapGL, {FlyToInterpolator} from "react-map-gl";
+import Geocoder from "react-map-gl-geocoder";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZnJvemllcyIsImEiOiJja3BrYWtqMHAwcWxwMndvZzJ3dTA0Mmx5In0.9hVe317rhGrT2ynl-1-bGQ';
+import { Editor, EditingMode, DrawLineStringMode, DrawPolygonMode } from 'react-map-gl-draw';
+import useCurrentLocation from "../hooks/useCurrentLocation";
+import {geolocationOptions} from "../constants/geolocationOptions";
+import useWatchLocation from "../hooks/useWatchLocation";
+
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZnJvemllcyIsImEiOiJja3BrYWtqMHAwcWxwMndvZzJ3dTA0Mmx5In0.9hVe317rhGrT2ynl-1-bGQ'
 
 const Map = () => {
-    // initializing the map container as a reference.
-    const mapContainerRef = useRef(null);
+    const mapRef = useRef();
 
-    // Latitude and longitude and zoom states for the map itself
-    const [lng, setLng] = useState(-81.8);
-    const [lat, setLat] = useState(26.5);
-    const [zoom, setZoom] = useState(10);
+    const [viewport, setViewport] = useState({
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 5
+    });
 
-    // Add area to a state that defaults to zero
-    const [area, setArea] = useState(0);
+    const MODES = [
+        { id: 'drawPolyline', text: 'Draw Polyline', handler: DrawLineStringMode },
+        { id: 'drawPolygon', text: 'Draw Polygon', handler: DrawPolygonMode },
+        { id: 'editing', text: 'Edit Feature', handler: EditingMode },
+    ];
 
-    // Create a mapbox drawing component
-    const Draw = new MapboxDraw();
+    const [mode, setMode] = useState(new DrawPolygonMode())
 
     //Location access
     const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
     const { location, cancelLocationWatch, error } = useWatchLocation(geolocationOptions);
     const [isWatchinForLocation, setIsWatchForLocation] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            goToUser();
+            console.log("went")
+        }, 3000);
+    },[location])
 
     // Retrieve the devices location
     useEffect(() => {
@@ -40,80 +52,66 @@ const Map = () => {
         }, 3000);
     }, [location, cancelLocationWatch]);
 
+    const handleViewportChange = useCallback(
+        (newViewport) => setViewport(newViewport),
+        []
+    );
 
-    // Initialize map when component mounts
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/frozies/ckpkaor1i10sl18p9bo2qkg49',
-            center: [lng, lat],
-            zoom: zoom
-        });
+    // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+    const handleGeocoderViewportChange = useCallback(
+        (newViewport) => {
+            const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
-        // Add navigation control (the +/- zoom buttons)
-        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-        map.addControl(Draw, 'top-left');
+            return handleViewportChange({
+                ...newViewport,
+                ...geocoderDefaultOverrides
+            });
+        },
+        [handleViewportChange]
+    );
 
-        map.on('move', () => {
-            setLng(map.getCenter().lng.toFixed(4));
-            setLat(map.getCenter().lat.toFixed(4));
-            setZoom(map.getZoom().toFixed(2));
-        });
-
-        map.on('draw.create', updateArea);
-        map.on('draw.delete', updateArea);
-        map.on('draw.update', updateArea);
-        map.on('load', setUserLocation);
-
-        function updateArea() { //TODO: Change to arrow function
-            const data = Draw.getAll();
-            if (data.features.length > 0) {
-                let area = turf.area(data);
-                // restrict to area to 2 decimal points
-                setArea(Math.round(area * 100) / 100);
-            } else {
-                setArea(0)
-            }
+    const goToUser = () => {
+        if (location) {
+            setViewport({
+                ...viewport,
+                longitude: location.longitude,
+                latitude: location.latitude,
+                zoom: 13,
+                transitionDuration: 3000,
+                transitionInterpolator: new FlyToInterpolator(),
+                // transitionEasing: d3.easeCubic
+            })
         }
 
-        function setUserLocation() {
-            setTimeout(() => {
-                if (location) {
-                    setLng(location.longitude);
-                    setLat(location.latitude);
-                    setZoom(15);
-                }
-            }, 250); // i need to do more testing to see if this timeout function is needed...
-        }
-
-        // Clean up on unmount
-        return () => map.remove();
-    }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
+    }
 
     return (
-        <div>
+        <div style={{ height: "100vh", width: '100vw' }}>
+            <MapGL
+                ref={mapRef}
+                {...viewport}
+                width="100%"
+                height="80%"
+                onViewportChange={handleViewportChange}
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+            >
+                <Geocoder
+                    mapRef={mapRef}
+                    onViewportChange={handleGeocoderViewportChange}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                    position="top-left"
+                />
 
-            {/*TODO: This is bad. move both of these imports somewhere else.*/}
-            <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.0/mapbox-gl.js'></script>
-            <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.0/mapbox-gl.css' rel='stylesheet' />
+                <Editor
+                    // // to make the lines/vertices easier to interact with
+                    clickRadius={12}
+                    mode={mode}
+                    position="top-right"
+                />
 
-            <link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.css' type='text/css' />
-            <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.3.0/mapbox-gl-draw.js'></script>
 
-            <div className='sidebarStyle'>
-                <div>
-                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Area: {area}m²
-                </div>
-
-                <p>Current position:</p>
-                <Location location={currentLocation} error={currentError} />
-
-                <p>Watch position: (Status: {isWatchinForLocation.toString()})</p>
-                <Location location={location} error={error} />
-            </div>
-            <div className='map-container' ref={mapContainerRef} />
+            </MapGL>
         </div>
     );
 };
-
-export default Map;
+export default Map
